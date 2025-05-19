6813bdfc419b5f9e2fad14679438541deb29409a
JTithy <tithyjebunnesa202@gmail.com>
2025-05-19 10:24:06 +0600

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+/*Convert Celcius to Fahrenheit*/
+#include<stdio.h>
+
+int main()
+
+{
+    float celcius, fahrenheit;
+    printf("Input the Celcius: \n");
+    scanf("%f", &celcius);
+    fahrenheit = (9.0/5.0)*celcius + 32;
+    printf("The Fahrenheit is: %2f\n", fahrenheit); 
+    return 0;
+}
