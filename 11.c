0b8241c97a833db19c1a484c5b10c7ebd97645ed
JTithy <tithyjebunnesa202@gmail.com>
2025-05-20 09:52:43 +0600

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,23 @@
+// Write a C program to find greatest of four numbers entered by the user.
+#include<stdio.h>
+
+int main()
+
+{
+    int a, b, c, d;
+    printf("Enter four numbers: \n");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if(a >= b && a >= c && a >= d){
+        printf("The greatest number is %d\n", a);
+    }
+    else if(b >= a && b >= c && b >= d){
+        printf("The greatest number is %d\n", b);
+    }
+    else if(c >= a && c >= b && c >= d){
+        printf("The greatest number is %d\n", c);
+    }
+    else{
+        printf("The greatest number is %d\n", d);
+    }
+    return 0;
+}
