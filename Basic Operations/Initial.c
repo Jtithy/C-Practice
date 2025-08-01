d5cc8c9f1e0d3dbd02d20a1dde4493f1930573e6
JTithy <tithyjebunnesa202@gmail.com>
2025-08-01 10:35:08 +0600

    Basic Operations/Initial.c

---
+++ Basic Operations/Initial.c
@@ -0,0 +1,10 @@
+//The variable 'a' is declared and initialized to 4. Write a program to print a, ++a, a++.
+#include<stdio.h>
+
+int main()
+
+{
+    int a=4;
+    printf("%d, %d, %d", a, ++a, a++);
+    return 0;
+}
