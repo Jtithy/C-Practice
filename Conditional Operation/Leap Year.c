322f78bd7a9dfb886a1e390b0de4907c1874998f
JTithy <tithyjebunnesa202@gmail.com>
2025-08-02 18:50:51 +0600

    Conditional Operation/Leap Year.c

---
+++ Conditional Operation/Leap Year.c
@@ -0,0 +1,18 @@
+// Write a program to find whether the year entered by the user is a leap year or not.
+#include<stdio.h>
+
+int main()
+
+{
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+    {
+        printf("The year is a leap year.\n");
+    }
+    else{
+        printf("The year is not a leap year.\n");
+    }
+    return 0;
+}
