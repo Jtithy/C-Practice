e4ed39a692653316b466ebd3c243ae420c0980e5
JTithy <tithyjebunnesa202@gmail.com>
2025-08-02 18:55:56 +0600

    Loop/Multiplication Table.c

---
+++ Loop/Multiplication Table.c
@@ -0,0 +1,15 @@
+//Write a program to print the multiplication table of the given number n.
+#include<stdio.h>
+
+int main()
+
+{
+    int N;
+    printf("Enter the number;\n");
+    scanf("%d", &N);
+    printf("Multiplication table of %d:\n", N);
+    for(int i=1; i<=10; i++){
+        printf("%d x %d = %d\n", N, i, N*i);
+    } 
+    return 0;
+}
